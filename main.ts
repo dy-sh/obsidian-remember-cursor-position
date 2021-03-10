3aa7611d628bce0c28cda16d26a415ebdde9bcd9
Derwish <derwish.pro@gmail.com>
2021-03-11 01:02:21 +0300

    save empheral state in view and edit mode

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, MarkdownView, View, MarkdownSubView } from 'obsidian';
 import * as CodeMirror from "codemirror";
 
 interface PluginSettings {
@@ -9,81 +9,77 @@ const DEFAULT_SETTINGS: PluginSettings = {
 	dbFileName: '.obsidian\\plugins\\obsidian-remember-cursor-position\\cursor-positions.json'
 }
 
-interface CursorPosition {
-	line: number;
-	ch: number;
+interface EphemeralState {
+	cursor?: {
+		from: {
+			ch: number
+			line: number
+		},
+		to: {
+			ch: number
+			line: number
+		}
+	}
+
+	scroll?: number
 }
 
-export default class CodeBlockFromSelection extends Plugin {
+
+export default class RememberCursorPosition extends Plugin {
 	settings: PluginSettings;
-	currentPos: CursorPosition;
+	lastEphemeralState: EphemeralState;
 
 	async onload() {
 		await this.loadSettings();
 
-		// this.addCommand({
-		// 	id: 'test-action1',
-		// 	name: 'Test',
-		// 	callback: () => this.saveScrollPosition(),
-		// 	hotkeys: [{ modifiers: ["Alt"], key: "1", },],
-		// });
-
-		// this.addCommand({
-		// 	id: 'test-action2',
-		// 	name: 'Test',
-		// 	callback: () => this.restoreScrollPosition(),
-		// 	hotkeys: [{ modifiers: ["Alt"], key: "2", },],
-		// });
-
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		this.registerEvent(
-			this.app.workspace.on('file-open', (file) => this.restoreScrollPosition()),
+			this.app.workspace.on('file-open', (file) => this.restoreEphemeralState()),
 		);
 
 		this.registerInterval(window.setInterval(() => this.checkPagePosition(), 500));
 	}
 
 	checkPagePosition() {
-		let pos = this.getCursorPosition();
+		let st = this.getEphemeralState();
 
-		if (!this.currentPos)
-			this.currentPos = pos;
+		if (!this.lastEphemeralState)
+			this.lastEphemeralState = st;
 
-		if (this.currentPos.ch != pos.ch || this.currentPos.line != pos.line) {
-			this.currentPos = pos;
-			this.saveScrollPosition()
+		if (!this.isEphemeralStatesEquals(st, this.lastEphemeralState)) {
+			this.lastEphemeralState = st;
+			this.saveEphemeralState(st)
 		}
 	}
 
-	async saveScrollPosition() {
-		let editor = this.getEditor();
+	isEphemeralStatesEquals(state1: EphemeralState, state2: EphemeralState): boolean {
+		return JSON.stringify(state1) === JSON.stringify(state2)
+	}
 
-		console.log(editor.getScrollInfo())
-
-		let pos = this.getCursorPosition();
+	async saveEphemeralState(state: EphemeralState) {
 		let fileName = this.app.workspace.getActiveFile().path.trim(); //this.app.workspace.activeLeaf.view.file.path.trim()
 
 		let db = await this.readDb();
-		db[fileName] = pos;
+		db[fileName] = state;
 		this.writeDb(db);
 	}
 
-	async restoreScrollPosition() {
+	async restoreEphemeralState() {
+		this.lastEphemeralState = {}
+
 		let db = await this.readDb();
-
 		let fileName = this.app.workspace.getActiveFile().path.trim();
-		let pos = db[fileName];
-		console.log(pos)
+		let st = db[fileName];
 
-		if (pos) {
-			this.scrollTo(pos.line)
-			this.setCursorPosition(pos);
+		if (st) {
+			this.setEphemeralState(st);
+			this.lastEphemeralState = st;
 		}
 	}
 
-	async readDb(): Promise<{ [file_path: string]: CursorPosition; }> {
-		let positions_dict: { [file_path: string]: CursorPosition; } = {}
+	async readDb(): Promise<{ [file_path: string]: EphemeralState; }> {
+		let positions_dict: { [file_path: string]: EphemeralState; } = {}
 
 		if (await this.app.vault.adapter.exists(this.settings.dbFileName)) {
 			let data = await this.app.vault.adapter.read(this.settings.dbFileName);
@@ -93,39 +89,22 @@ export default class CodeBlockFromSelection extends Plugin {
 		return positions_dict;
 	}
 
-	async writeDb(db: { [file_path: string]: CursorPosition; }) {
+	async writeDb(db: { [file_path: string]: EphemeralState; }) {
 		await this.app.vault.adapter.write(this.settings.dbFileName, JSON.stringify(db));
 	}
 
 
-	getCursorPosition(): CursorPosition {
-		let editor = this.getEditor();
-		let cursor = editor.getCursor();
 
-		return {
-			line: cursor.line,
-			ch: cursor.ch
-		};
+
+	getEphemeralState(): EphemeralState {
+		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		return view.getEphemeralState();
 	}
 
-	setCursorPosition(pos: CursorPosition) {
-		let editor = this.getEditor();
-		editor.setCursor(pos.line, pos.ch);
+	setEphemeralState(state: EphemeralState) {
+		const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+		view.setEphemeralState(state)
 	}
-
-	scrollTo(line: number) {
-		let editor = this.getEditor();
-
-		var t = editor.charCoords({ line: line, ch: 0 }, "local").top;
-		var middleHeight = editor.getScrollerElement().offsetHeight / 2;
-		editor.scrollTo(null, t - middleHeight - 5);
-	}
-
-	private getEditor(): CodeMirror.Editor {
-		let activeLeaf: any = this.app.workspace.activeLeaf;
-		return activeLeaf.view.sourceMode.cmEditor;
-	}
-
 
 
 	async loadSettings() {
@@ -140,9 +119,9 @@ export default class CodeBlockFromSelection extends Plugin {
 
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: CodeBlockFromSelection;
+	plugin: RememberCursorPosition;
 
-	constructor(app: App, plugin: CodeBlockFromSelection) {
+	constructor(app: App, plugin: RememberCursorPosition) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
